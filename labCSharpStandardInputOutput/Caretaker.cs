6f9ce74bc885571bad1d22bfe65d7c82511132f2
inkoromi21 <sema.vorobev20078@gmail.com>
2026-03-21 15:09:43 +0700

    исправляю

---
+++ labCSharpStandardInputOutput/Caretaker.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextFileEditor
+{
+  public class Caretaker
+  {
+    private Stack<object> _undoStack;
+    private Stack<object> _redoStack;
+
+    public Caretaker()
+    {
+      _undoStack = new Stack<object>();
+      _redoStack = new Stack<object>();
+    }
+
+    public void SaveState(IOriginator originator)
+    {
+      object memento;
+
+      memento = originator.GetMemento();
+      _undoStack.Push(memento);
+    }
+
+    public void Undo(IOriginator originator)
+    {
+      bool canUndo;
+      object memento;
+
+      canUndo = _undoStack.Count > 0;
+
+      if (canUndo)
+      {
+        memento = originator.GetMemento();
+        _redoStack.Push(memento);
+
+        memento = _undoStack.Pop();
+        originator.SetMemento(memento);
+      }
+    }
+
+    public void Redo(IOriginator originator)
+    {
+      bool canRedo;
+      object memento;
+
+      canRedo = _redoStack.Count > 0;
+
+      if (canRedo)
+      {
+        memento = originator.GetMemento();
+        _undoStack.Push(memento);
+
+        memento = _redoStack.Pop();
+        originator.SetMemento(memento);
+      }
+    }
+
+    public void ClearRedo()
+    {
+      _redoStack.Clear();
+    }
+
+    public void Clear()
+    {
+      _undoStack.Clear();
+      _redoStack.Clear();
+    }
+
+    public List<TextEditorMemento> GetHistory()
+    {
+      List<TextEditorMemento> historyList;
+      object[] historyArray;
+      int stateIndex;
+      TextEditorMemento memento;
+
+      historyList = new List<TextEditorMemento>();
+      historyArray = _undoStack.ToArray();
+
+      for (stateIndex = 0; stateIndex < historyArray.Length; ++stateIndex)
+      {
+        memento = historyArray[stateIndex] as TextEditorMemento;
+
+        if (memento != null)
+        {
+          historyList.Add(memento);
+        }
+      }
+
+      return historyList;
+    }
+  }
+}
